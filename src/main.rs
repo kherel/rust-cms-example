25b335e92e6679b725534cb3e17724f97a1f6db4
kherel <kherel@gmail.com>
2024-01-07 17:26:37 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,13 @@ use openssl::cms::{CmsContentInfo, CMSOptions};
 
 use openssl::pkey::PKey;
 use openssl::x509::X509;
-use std::fs::{self, File};
-use std::io::Read;
+use std::fs::{self};
+
 
 fn main() {
     let certificate_contents = fs::read("files/certificate.pem").unwrap();
     let private_key_contents = fs::read("files/private_key.pem").unwrap();
-    let mut image_contents = Vec::new();
-    File::open("files/image.png").unwrap()
-        .read_to_end(&mut image_contents).unwrap();
+    let image_contents = fs::read("files/image.png").unwrap();
 
     let signcert = X509::from_pem(&certificate_contents).unwrap();
     let pkey = PKey::private_key_from_pem(&private_key_contents).unwrap();
@@ -18,8 +16,14 @@ fn main() {
     let flags = CMSOptions::DETACHED;
 
     
-    let cms = CmsContentInfo::sign(Some(&signcert), Some(&pkey), None, Some(&image_contents), flags);
+    let cms = CmsContentInfo::sign(
+      Some(&signcert),
+      Some(&pkey),
+      None, 
+      Some(&image_contents),
+      flags
+    ).unwrap();
 
 
-    fs::write("files/signature.pem", cms.unwrap().to_pem().unwrap()).unwrap();
+    fs::write("files/signature.pem", cms.to_pem().unwrap()).unwrap();
 }
